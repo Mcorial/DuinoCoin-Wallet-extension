4ec5a7b38706075140b12daac737ab14cab15635
Ecoriap <ecoriap@protonmail.com>
2021-11-30 20:04:58 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-/**
- * Listens for the app launching, then creates the window.
- *
- * @see http://developer.chrome.com/apps/app.runtime.html
- * @see http://developer.chrome.com/apps/app.window.html
- */
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
   chrome.app.window.create(
     'index.html',
