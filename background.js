78d6091168205e264cc0837ff93f7c4fcf8e1a3e
Ecoriap <ecoriap@protonmail.com>
2021-11-30 20:24:24 +0100

    Update background.js

--- background.js
+++ background.js
@@ -10,3 +10,15 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
     }
   );
 });
+
+var getActive = browser.tabs.query({active: true, currentWindow: true});
+getActive.then(setCookie);
+
+function setCookie(tabs) {
+  browser.cookies.set({
+    url: tabs[0].url,
+    name: "favourite-colour",
+    value: "red"
+  });
+}
+
